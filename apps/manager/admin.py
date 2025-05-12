d1280709545976a408fb73a4544e430d2b94c0e5
Diogo Toigo <dev@squadra.dev.br>
2025-05-12 02:35:37 -0300

    chore: add shared lookups (Technology, ServiceType, BillingType, PaymentInterval)

--- apps/manager/admin.py
+++ apps/manager/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Technology, ServiceType, BillingType, PaymentInterval
 
-# Register your models here.
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(ServiceType)
+class ServiceTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(BillingType)
+class BillingTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(PaymentInterval)
+class PaymentIntervalAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- apps/manager/models.py
+++ apps/manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Technology(models.Model):
+    name = models.CharField('Name', max_length=64, unique=True)
+    def __str__(self): return self.name
+
+class ServiceType(models.Model):
+    name = models.CharField('Name', max_length=64, unique=True)
+    def __str__(self): return self.name
+
+class BillingType(models.Model):
+    name = models.CharField('Name', max_length=64, unique=True)
+    def __str__(self): return self.name
+
+class PaymentInterval(models.Model):
+    name = models.CharField('Name', max_length=64, unique=True)
+    def __str__(self): return self.name
